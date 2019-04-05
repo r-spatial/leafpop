c0088f6242236636e3c1b9b5e78a2b0c7d44d7f9
tim-salabim <tim.appelhans@gmail.com>
2019-04-05 21:15:35 +0200

    initial commit

---
+++ R/leafpop-package.R
@@ -0,0 +1,19 @@
+#' Include Tables, Images and Graphs in Leaflet Popups
+#'
+#' Creates HTML strings to embed tables, images or graphs in popups
+#' of interactive maps created with packages 'leaflet' or 'mapview'. Handles
+#' local paths to images on the file system or remote urls. Handles graphs created
+#' with 'base' graphics, 'lattice' or 'ggplot2' as well as interactive plots created
+#' with 'htmlwidgets'.
+#'
+#' @name leafpop-package
+#' @docType package
+#' @title Include Tables, Images and Graphs in Leaflet Popups
+#' @author Tim Appelhans, Florian Detsch
+#' \emph{Maintainer:} Tim Appelhans \email{tim.appelhans@gmail.com}
+#'
+#' @keywords package
+#'
+#' @rawNamespace useDynLib(leafpop, .registration = TRUE)
+#'
+NULL
