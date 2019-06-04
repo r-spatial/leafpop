5f7cd8c549c8792f79f93af42361df56ac6ae30e
tim-salabim <tim.appelhans@gmail.com>
2019-06-04 23:07:17 +0200

    getting somewhere. registering images works. #1

--- R/addImagePopup.R
+++ R/addImagePopup.R
@@ -1,4 +1,43 @@
-addImagePopup = function(map, image, group) {
+addPopupImage = function(map, img, group, width = NULL, height = NULL) {
+
+  drs = file.path(tempdir(), "images")
+  if (!dir.exists(drs)) dir.create(drs)
+
+  pngs = lapply(1:length(img), function(i) {
+
+    fl = img[[i]]
+
+    # info = strsplit(
+    #   sf::gdal_utils(
+    #     util = "info",
+    #     source = fl,
+    #     quiet = TRUE
+    #   ),
+    #   split = "\n"
+    # )
+    info = sapply(fl, function(...) gdalUtils::gdalinfo(...))
+    info = unlist(lapply(info, function(i) grep(utils::glob2rx("Size is*"), i, value = TRUE)))
+    cols = as.numeric(strsplit(gsub("Size is ", "", info), split = ", ")[[1]])[1]
+    rows = as.numeric(strsplit(gsub("Size is ", "", info), split = ", ")[[1]])[2]
+    yx_ratio = rows / cols
+    xy_ratio = cols / rows
+
+    if (is.null(height) && is.null(width)) {
+      width = 300
+      height = yx_ratio * width
+    } else if (is.null(height)) {
+      height = yx_ratio * width
+    } else if (is.null(width)) {
+      width = xy_ratio * height
+    }
+
+    nm = basename(fl)
+    fls = file.path(drs, nm)
+    invisible(file.copy(fl, file.path(drs, nm)))
+
+    return(nm)
+
+  })
 
   map$dependencies <- c(
     map$dependencies,
@@ -9,6 +48,14 @@ addImagePopup = function(map, image, group) {
         system.file("htmlwidgets", package = "leafpop"),
         script = c("popup.js")
       )
+    ),
+    list(
+      htmltools::htmlDependency(
+        "image",
+        "0.0.1",
+        drs,
+        attachment = unlist(pngs)
+      )
     )
   )
 
@@ -16,7 +63,7 @@ addImagePopup = function(map, image, group) {
     map,
     leaflet::getMapData(map),
     'imagePopup',
-    image,
+    pngs,
     group
   )
 }
--- inst/htmlwidgets/popup.js
+++ inst/htmlwidgets/popup.js
@@ -1,6 +1,17 @@
 LeafletWidget.methods.imagePopup = function(image, group) {
 
+
   var lay = this.layerManager._groupContainers[group];
-  lay.bindPopup(image, { maxWidth: 2000 });
+
+  var img = [];
+  for (i = 0; i < image.length; i++) {
+    var id = i + 1;
+    id = id.toString();
+    img[i] = document.getElementById("image" + "-" + id + "-attachment").href;
+  }
+
+  //debugger;
+
+  lay.bindPopup("<image src='" + img + "'>", { maxWidth: 2000 });
 
 };
