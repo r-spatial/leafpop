a5a44d48e097a82dd3e519571fb062444d272777
tim-salabim <tim.appelhans@gmail.com>
2019-06-26 22:19:53 +0200

    first try at addPopupVideos. Only working if width and/or height are supplied.

--- inst/htmlwidgets/popup.js
+++ inst/htmlwidgets/popup.js
@@ -47,6 +47,55 @@ LeafletWidget.methods.imagePopup = function(image, group, width, height, src, na
 };
 
 
+LeafletWidget.methods.videoPopup = function(video, group, width, height, name) {
+
+  var lay = this.layerManager._groupContainers[group];
+
+  //debugger;
+
+  var vdo = [];
+  var vid = [];
+
+  for (i = 0; i < video.length; i++) {
+    if (name.length === 1) {
+      vdo[i] = document.getElementById("video" + "-" + name + "-attachment").href;
+    } else {
+      vdo[i] = document.getElementById("video" + "-" + name[i] + "-attachment").href;
+    }
+
+    vid[i] = document.createElement('video');
+    vid[i].src = vdo[i];
+
+    if (width[i] === null & height[i] === null) {
+      width[i] = vid[i].videoWidth;
+      height[i] = vid[i].videoHeight;
+    } else if (width[i] === null) {
+      xy_ratio = vid[i].videoWidth / vid[i].videoHeight;
+      width[i] = xy_ratio * height[i];
+    } else if (height[i] === null) {
+      xy_ratio = vid[i].videoHeight / vid[i].videoWidth;
+      height[i] = xy_ratio * width[i];
+    }
+  }
+
+  //debugger;
+
+  var vdoid = 0;
+  lay.eachLayer(function (layer) {
+    wdth = width[vdoid];
+    hght = height[vdoid];
+    if (vdoid <= video.length) {
+      pop = "<video controls src='" + vid[vdoid].src + "'" + " height=" + hght + " width=" + wdth + ">";
+      layer.bindPopup(pop, { maxWidth: 2000 });
+    }
+    //debugger;
+    vdoid += 1;
+  });
+
+};
+
+
+
 
 LeafletWidget.methods.iframePopup = function(source, group, width, height, src, name) {
 
