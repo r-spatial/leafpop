8ab2c11e3ca716ee732582c09357d17682a5710f
fdetsch <fdetsch@web.de>
2019-08-03 10:54:05 -0700

    Renamed file.

--- R/brewRy.R
+++ R/brewRy.R
@@ -1,36 +1,3 @@
-# StringVector listPopupTemplates(CharacterMatrix x, CharacterVector names,
-#                                 std::string tmpPath, bool rowIndex) {
-#
-#   // number of rows and columns
-#   int nRows = x.nrow();
-#   int nCols = x.ncol();
-#
-#   // intermediary variables
-#   CharacterVector chVal(nCols);
-#   std::string chStr;
-#
-#   // output list
-#   StringVector lsOut(nRows);
-#
-#   // import template
-#   std::string chTemplate = createTemplate(tmpPath);
-#   String chTmp = chTemplate;
-#
-#   // create strings for each single row
-#   for (int i = 0; i < nRows; i++) {
-#     chVal = enc2utf8_chrvec(x(i, _));
-#     chStr = mergePopupRows(names, chVal, rowIndex);
-#
-#     chTmp = gsubC("<%=pop%>", chStr, chTmp);
-#     lsOut[i] = enc2utf8_string(chTmp);
-#
-#     // reset intermediary string
-#     chTmp = chTemplate;
-#   }
-#
-#   return lsOut;
-# }
-
 listPopupTemplatesR = function(x, names, row_index = TRUE) {
 
   id_crd = names == "Feature ID"
@@ -69,12 +36,8 @@ brewPopupRowR = function(index, colname, value, row_index = TRUE) {
   col_string = paste0("<td><b>", colname, "&emsp;</b></td>")
   val_string = paste0("<td align='right'>", value, "&emsp;</td>")
 
-  # classes = rep(ifelse(index %% 2 == 0, " class=\'alt\'>", ">")
-  #               , each = ratio <- length(val_string) / length(index))
-  # indexes = rep(ind_string, each = ratio)
-  # colnames = rep(col_string, each = ratio)
-
-  out_string = paste0("<tr", ifelse(index %% 2 == 0, " class=\'alt\'>", ">"), ind_string, col_string, val_string, "</tr>")
+  out_string = paste0("<tr", ifelse(index %% 2 == 0, " class=\'alt\'>", ">")
+                      , ind_string, col_string, val_string, "</tr>")
 
   return(out_string)
 }
@@ -99,13 +62,3 @@ createTemplateR = function() {
 </html>
 ')
 }
-
-# mergePopupRowsR = function(names, values, row_index = TRUE, id_crd, id_val = !id_crd) {
-#
-#   out = character(length(names))
-#   out[id_crd] = brewPopupCoordsR(names[id_crd], values[id_crd])
-#   out[id_val] = brewPopupRowR(which(id_val), names[id_val], values[id_val], row_index)
-#
-#   return(paste(out, collapse = ""))
-# }
-
