b429acdd470718b09ced897e831104159af82b29
tim-salabim <tim.appelhans@gmail.com>
2019-09-07 15:21:37 +0200

    use sprintf instead of paste(0)

--- R/brewRy.R
+++ R/brewRy.R
@@ -16,31 +16,50 @@ listPopupTemplates = function(x, row_index = TRUE) {
   args = c(out, sep = "")
   out = do.call(paste, args)
 
-  as.character(vsub("<%=pop%>", out, createTemplate()))
+  # as.character(vsub("<%=pop%>", out, createTemplate(), perl = TRUE))
+  sprintf(createTemplate(), out)
+
+  # vapply(out, gsub, "chracter", x = createTemplate()
+  #        , pattern = "<%=pop%>", USE.NAMES = FALSE)
 }
 
 vsub = Vectorize(gsub)
 
 brewPopupCoords = function(colname, value) {
   ind_string = "<td></td>"
-  col_string = paste0("<td><b>", colname, "</b></td>")
-  val_string = paste0("<td align='right'>", value, "&emsp;</td>")
-  out_string = paste0("<tr class='coord'>", ind_string, col_string, val_string, "</tr>")
+  # col_string = paste0("<td><b>", colname, "</b></td>")
+  # val_string = paste0("<td align='right'>", value, "&emsp;</td>")
+  # out_string = paste0("<tr class='coord'>", ind_string, col_string, val_string, "</tr>")
+  col_string = sprintf("<td><b>%s</b></td>", colname)
+  val_string = sprintf("<td align='right'>%s&emsp;</td>", value)
+  out_string = sprintf("<tr class='coord'>%s%s%s</tr>", ind_string, col_string, val_string)
 
   return(out_string)
 }
 
 brewPopupRow = function(index, colname, value, row_index = TRUE) {
+  # ind_string = if (row_index) {
+  #   paste0("<td>", index - 1, "</td>")
+  # } else {
+  #   "<td></td>"
+  # }
+  # col_string = paste0("<td><b>", colname, "&emsp;</b></td>")
+  # val_string = paste0("<td align='right'>", value, "&emsp;</td>")
+  #
+  # out_string = paste0("<tr", ifelse(index %% 2 == 0, " class=\'alt\'>", ">")
+  #                     , ind_string, col_string, val_string, "</tr>")
   ind_string = if (row_index) {
-    paste0("<td>", index - 1, "</td>")
+    sprintf("<td>%s</td>", index - 1)
   } else {
     "<td></td>"
   }
-  col_string = paste0("<td><b>", colname, "&emsp;</b></td>")
-  val_string = paste0("<td align='right'>", value, "&emsp;</td>")
+  col_string = sprintf("<td><b>%s&emsp;</b></td>", colname)
+  val_string = sprintf("<td align='right'>%s&emsp;</td>", value)
 
-  out_string = paste0("<tr", ifelse(index %% 2 == 0, " class=\'alt\'>", ">")
-                      , ind_string, col_string, val_string, "</tr>")
+  row_class = paste0("<tr", ifelse(index %% 2 == 0, " class=\'alt\'>%s%s%s</tr>",
+                                   ">%s%s%s</tr>"))
+
+  out_string = sprintf(row_class, ind_string, col_string, val_string)
 
   return(out_string)
 }
@@ -57,7 +76,7 @@ createTemplate = function() {
 <div class="scrollableContainer">
 <table class="popup scrollable" id="popup">
 
-<%=pop%>
+%s
 
 </table>
 </div>
